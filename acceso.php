68c36d3e01df25a2188502479491af7b4609a1fd
fernandoamz <fernandoamz148@gmail.com>
2015-04-06 16:31:54 -0500

    nuevo-proyecto

--- acceso.php
+++ acceso.php
@@ -11,7 +11,6 @@
 
         <!-- CSS -->
         <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=PT+Sans:400,700'>
-        <link rel="stylesheet" href="assets/css/celular.css">
         <link rel="stylesheet" href="assets/css/reset.css">
         <link rel="stylesheet" href="assets/css/supersized.css">
         <link rel="stylesheet" href="assets/css/style.css">
